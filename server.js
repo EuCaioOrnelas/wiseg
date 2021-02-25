c63d4829db133b92f66ad247c8a5c711146bee94
Mailon Ruan <maailon@msn.com>
2021-02-25 11:54:44 -0300

    add cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const moment = require('moment')
 const axios = require('axios')
-
 const express = require('express')
+var cors = require('cors')
+
 const app = express()
+app.use(cors())
 
 const http = require('http').Server(app)
 
